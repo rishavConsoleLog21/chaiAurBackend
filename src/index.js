202d6859bf02143925f02017a1811643728621d6
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-08-25 00:23:50 +0530

    Add utils function & DB connectoin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port: ${process.env.PORT}`);
+    });
+    app.on("Error", (error) => {
+      console.log("Error While connecting MongoDB", error);
+      throw error;
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Failed", error);
+  });
 
 /*
 import express from "express";
--- src/utils/ApiRessponse.js
+++ src/utils/ApiRessponse.js
@@ -6,3 +6,5 @@ class ApiRessponse {
     this.success = statusCode < 400;
   }
 }
+
+export { ApiRessponse };
