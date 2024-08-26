d740a619a23f6ccfd834ba984435cca944bf4bb2
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-08-26 23:29:24 +0530

    Add Cloudinay & Multer Middleware

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,18 @@
+//middelware: if you are going somewhere meet me and then go
+import multer from "multer";
+import { nanoid } from "nanoid/non-secure";
+
+const id = nanoid();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cd) {
+    cd(null, file.originalname + nanoid);
+    console.log(cd);
+  },
+});
+
+export const upload = multer({
+  storage,
+});
