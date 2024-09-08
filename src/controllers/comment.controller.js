20dd1b799f6b5b6bda36eea10c9284aa9783c20a
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-09-08 21:43:31 +0530

    Add comment controller to fetch, add, update, and delete comments

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,31 +1,100 @@
-import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.models0000000000.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+  //TODO: get all comments for a video
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
 
-})
+  if (!mongoose.isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video ID");
+  }
+
+  const comments = await Comment.find({ video: videoId })
+    .populate("user", "username avatar")
+    .sort({ createdAt: -1 })
+    .limit(limit * 1)
+    .skip((page - 1) * limit);
+
+  if (!comments) {
+    throw new ApiError(404, "No comments found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comments, "All comments fetched successfully"));
+});
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
-})
+  // TODO: add a comment to a video
+  const { videoId } = req.params;
+  const { comment } = req.body;
+
+  if (!mongoose.isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video ID");
+  }
+
+  const newComment = new Comment({
+    comment,
+    user: req.user._id,
+    video: videoId,
+  });
+
+  const savedComment = await newComment.save();
+
+  if (!savedComment) {
+    throw new ApiError(400, "Comment not saved");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, savedComment, "Comment added successfully"));
+});
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
-})
+  // TODO: update a
+  const { commentId } = req.params;
+  const { comment } = req.body;
+
+  if (!mongoose.isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid comment ID");
+  }
+
+  const updatedComment = await Comment.findByIdAndUpdate(
+    commentId,
+    { comment },
+    { new: true }
+  );
+
+  if (!updatedComment) {
+    throw new ApiError(400, "Comment not updated");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedComment, "Comment updated successfully"));
+});
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
-})
+  // TODO: delete a comment
+  const { commentId } = req.params;
 
-export {
-    getVideoComments, 
-    addComment, 
-    updateComment,
-     deleteComment
-    }
+  if (!mongoose.isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid comment ID");
+  }
+
+  const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+  if (!deletedComment) {
+    throw new ApiError(400, "Comment not deleted");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Comment deleted successfully"));
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
